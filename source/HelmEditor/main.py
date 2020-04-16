319716dc73a29a50cb64c7d615e02bd2f37510c8
Bob Wang <wangziling1000@gmail.com>
2020-04-16 10:43:35 +0200

    set the project directory

---
+++ source/HelmEditor/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+main.py
+"""
+
+
+class YamlParser:
+    def __init__(self):
+        pass
+
+
+def main():
+    print('hello')
+
+
+if __name__ == '__main__':
+    main()
